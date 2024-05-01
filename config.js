249fce878a2a2e13db645330d7f6819cdd0ed131
Alberlegrand <75179569+Alberlegrand@users.noreply.github.com>
2024-04-30 20:48:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.location="Port-au-Prince,Haiti."
 global.mongodb= process.env.MONGODB_URI || ""
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
-global.DATABASE_URL = process.env.DATABASE_URL || ""
+global.DATABASE_URL = process.env.DATABASE_URL || "postgres://mrmvwocqghemeb:5f6159fca8cc7990e8c443e3db9c370d294fec3e908e1e5b79b18d7ccf25c662@ec2-35-169-11-108.compute-1.amazonaws.com:5432/dbanphjqb7q20l"
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
@@ -72,10 +72,10 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.4",
-  caption : process.env.CAPTION || "©sᴜʜᴀɪʟ²²¹-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
+  caption : process.env.CAPTION || "©HAITIAN IT Developer²²¹-Bot" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ HAITIAN IT Developer²²¹-Bot 』``` //*『sᴜʙsᴄʀɪʙᴇ • HAITIAN IT Developer ᴛᴇᴄʜ』*\n"),
  
-  author : process.env.PACK_AUTHER|| "",
-  packname: process.env.PACK_NAME || "",
+  author : process.env.PACK_AUTHER|| "Mr Alberno",
+  packname: process.env.PACK_NAME || "HITDEV",
   botname : process.env.BOT_NAME  || "HITDEV-BOT",
   ownername:process.env.OWNER_NAME|| "Mr Alberno",
 
@@ -85,8 +85,8 @@ module.exports = {
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "HRKU-b3a40199-04b9-40cb-9d1e-53dda83bdf41",
+  HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "nuera9807hitai",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
 
